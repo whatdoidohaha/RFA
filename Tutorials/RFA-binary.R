d23e76365144f4461fc123e9549d9858b5c32a2a
Yeonwoo Park <140142337+whatdoidohaha@users.noreply.github.com>
2023-08-08 16:58:46 +0900

    Update RFA-binary.R

--- Tutorials/RFA-binary.R
+++ Tutorials/RFA-binary.R
@@ -24,7 +24,7 @@
 # Settings (recommended not to change in a run) ---------------------------
 
 # Set working directory.
-setwd('/Users/yeonwoopark/Desktop/RFA/Examples/')
+setwd('/Users/yeonwoopark/Desktop/RFA/Tutorials/')
 
 # Source the script RFA-binary-functions.R to import function definitions.
 source('../Scripts/RFA-binary-functions.R')
