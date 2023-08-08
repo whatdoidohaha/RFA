3d2ce03ccf4f6b971585711d98e5d5d0415f88c4
Yeonwoo Park <140142337+whatdoidohaha@users.noreply.github.com>
2023-08-08 16:59:06 +0900

    Update RFA.R

--- Tutorials/RFA.R
+++ Tutorials/RFA.R
@@ -23,7 +23,7 @@
 # Settings (recommended not to change in a run) ---------------------------
 
 # Set working directory.
-setwd('/Users/yeonwoopark/Desktop/RFA/Examples/')
+setwd('/Users/yeonwoopark/Desktop/RFA/Tutorials/')
 
 # Source the script RFA-functions.R to import function definitions.
 source('../Scripts/RFA-functions.R')
