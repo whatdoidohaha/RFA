0714a2b8d9eef1c5fb430e7d168dcbb8ef15784f
Yeonwoo Park <140142337+whatdoidohaha@users.noreply.github.com>
2023-09-01 08:58:28 +0900

    Add files via upload

---
+++ Paper/analyses/CH65-G189E/sparsity.R
@@ -0,0 +1,31 @@
+
+# Reference-free analysis modeling nonspecific epistasis; sparsity analysis.
+# Binary state space; using the script ref_free_binary.R.
+
+
+# Data import -------------------------------------------------------------
+
+source('../../scripts/ref_free_binary.R')
+dataset <- 'CH65-G189E'
+
+# Control file:
+#   1. Directory to the genotype matrix.
+#   2. Directory to the phenotype vector.
+#   3. Model order (or directory to a site-combination list).
+#   4. Type of nonspecific epistasis.
+control <- list(paste0('../../data/', dataset, '/genotypes.txt'), paste0('../../data/', dataset, '/phenotypes.txt'), '1', 2)
+set_model(control)
+
+# Model to determine the order of parameter expansion.
+model <- import_model('regularized_fit_3rd.txt')
+
+
+# Setting and run ---------------------------------------------------------
+
+# Five-fold cross-validation is performed to calculate the out-of-sample R2 of each model.
+k <- 5L
+lambda_list <- 10 ^ seq(-3, 1, by = 1)
+
+analyze_sparsity(genotypes, y, model, site_combn, k, lambda_list)
+# Output is written in a .txt file named sparsity.
+
---
+++ Paper/analyses/beta-lactamase/sparsity.R
@@ -0,0 +1,34 @@
+
+# Reference-free analysis modeling nonspecific epistasis; sparsity analysis.
+# Cross-validation is performed across replicate measurements.
+# Binary state space; using the script ref_free_binary.R.
+
+
+# Data import -------------------------------------------------------------
+
+source('../../scripts/ref_free_binary.R')
+dataset <- 'beta-lactamase'
+
+# Control file:
+#   1. Directory to the genotype matrix.
+#   2. Directory to the phenotype vector.
+#   3. Model order (or directory to a site-combination list).
+#   4. Type of nonspecific epistasis.
+control <- list(paste0('../../data/', dataset, '/genotypes.txt'), paste0('../../data/', dataset, '/phenotypes.txt'), '1', 2)
+set_model(control)
+
+# Model to determine the order of parameter expansion.
+model <- import_model('regularized_fit_2nd.txt')
+
+# Phenotype replicates.
+replicates <- as.matrix(read.csv(paste0('../../data/', dataset, '/phenotype_replicates.txt'), header = TRUE))
+
+
+# Setting and run ---------------------------------------------------------
+
+# Cross-validation is performed across measurement replicates.
+lambda_list <- 10 ^ seq(-3, 1, by = 1)
+
+analyze_sparsity_replicates(genotypes, replicates, model, site_combn, lambda_list)
+# Output is written in a .txt file named sparsity.
+
