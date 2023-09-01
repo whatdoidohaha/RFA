d463e1c84c0e5f922251c8f00b879f8879a0b12b
Yeonwoo Park <140142337+whatdoidohaha@users.noreply.github.com>
2023-09-01 09:00:41 +0900

    Add files via upload

---
+++ Paper/analyses/CR9114-B/prediction_from_subsample.R
@@ -0,0 +1,45 @@
+
+# Reference-free analysis modeling nonspecific epistasis; prediction from subsample.
+# Binary state space; using the script ref_free_binary.R.
+
+
+# Data import -------------------------------------------------------------
+
+source('../../scripts/ref_free_binary.R')
+dataset <- 'CR9114-B'
+
+# Control file:
+#   1. Directory to the genotype matrix.
+#   2. Directory to the phenotype vector.
+#   3. Model order (or directory to a site-combination list).
+#   4. Type of nonspecific epistasis.
+control <- list(paste0('../../data/', dataset, '/genotypes.txt'), paste0('../../data/', dataset, '/phenotypes.txt'), '1', 2)
+set_model(control)
+
+
+# Setting -----------------------------------------------------------------
+
+# Model orders to test.
+# The model that best describes the subsample is used for predicting the phenotypes of all other genotypes.
+order_list <- c(1L)
+
+# Sample sizes to test (fraction of all possible genotypes, not that of experimentally characterized genotypes).
+frac_list <- seq(0.1, 0.8, by = 0.1)
+
+# Number of samples to try for each sample size.
+niter <- 10L
+
+# Regularization strengths.
+lambda_list <- 10 ^ seq(-3, 2, by = 1)
+
+# Three-fold cross-validation within the subsample to infer the best model.
+CV_k <- 3L
+
+
+# Run ---------------------------------------------------------------------
+
+prediction <- predict_from_subsample(genotypes, y, order_list, frac_list, niter, lambda_list, CV_k)
+# For analyzing this dataset, epsilon for cross-validation was set as 1e-5 instead of 1e-2.
+# The strong lower bounding in this datset requires greater precision to avoid local optima.
+
+write.csv(round(t(prediction), 3L), 'prediction.txt', row.names = FALSE)
